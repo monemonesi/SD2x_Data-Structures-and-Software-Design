cf6eda95699e11d6dad87c0adb979b078c7bfd71
Roberto Monesi <mone.monesi.mm@gmail.com>
2018-03-07 23:56:09 +0000

    HW5: TreeMap<String,PriorityQueue> parseMovieRating has been added

--- HW/MovieRatingsParser.java
+++ HW/MovieRatingsParser.java
@@ -1,3 +1,4 @@
+
 /*
  * SD2x Homework #5
  * Implement the method below according to the specification in the assignment description.
@@ -9,12 +10,57 @@ import java.util.PriorityQueue;
 import java.util.TreeMap;
 
 public class MovieRatingsParser {
+    /*
+     * Takes a List of UserMovieRatings as input and creates a TreeMap data
+     * structure from it. The TreeMap is a mapping from the movie’s title to a
+     * PriorityQueue of all its associated ratings.
+     * 
+     * If the input List is null or empty, parseMovieRatings should return an empty
+     * TreeMap.
+     * 
+     * If the input List contains any null UserMovieRatings object, or a
+     * UserMovieRatings object whose movie title is null or an empty string, or a
+     * UserMovieRatings object whose rating is negative, parseMovieRatings should
+     * ignore that UserMovieRatings object
+     * 
+     * The movie titles should be considered case-insensitive, i.e. if two
+     * UserMovieRatings objects have the same title that differ only in case (upper
+     * or lower), they should be considered the same movie. The movie titles stored
+     * in the TreeMap must use lowercase letters.
+     */
 
-	public static TreeMap<String, PriorityQueue<Integer>> parseMovieRatings(List<UserMovieRating> allUsersRatings) {
+    public static TreeMap<String, PriorityQueue<Integer>> 
+    parseMovieRatings(List<UserMovieRating> allUsersRatings) {
+
+	TreeMap<String, PriorityQueue<Integer>> map = new TreeMap<>();
+	//If the input List is null or empty, parseMovieRatings should return an empty TreeMap.
+	if(allUsersRatings == null || allUsersRatings.isEmpty()) return map;
+	
+	for (UserMovieRating rating : allUsersRatings) {
+	    /*
+	     * If the input List contains any null UserMovieRatings object, or a
+	     * UserMovieRatings object whose movie title is null or an empty string, or a
+	     * UserMovieRatings object whose rating is negative, parseMovieRatings should
+	     * ignore that UserMovieRatings object
+	     */
+	    if(rating != null && rating.getMovie() != null && !rating.getMovie().isEmpty()
+		    && rating.getUserRating()>=0) {
 		
-		/* IMPLEMENT THIS METHOD! */
-		
-		return null; // this line is here only so this code will compile if you don't modify it
+		String movieTitle = rating.getMovie().toLowerCase();
+		if(map.containsKey(movieTitle)) {
+		    //the movietitle is already in my map
+		    map.get(movieTitle).add(rating.getUserRating());
+		}else {
+		    PriorityQueue<Integer> allRatingsForMovie = new PriorityQueue<>();
+		    allRatingsForMovie.add(rating.getUserRating());
+		    map.put(movieTitle, allRatingsForMovie);
+		}
+	    }
+	    
 	}
+	
+	
+	return map;
+    }
 
 }
