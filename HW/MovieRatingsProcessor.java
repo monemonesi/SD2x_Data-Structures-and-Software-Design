f9cf05eb7f0adb3a0a3479c694bea5fcffcc88d7
Roberto Monesi <mone.monesi.mm@gmail.com>
2018-03-08 00:25:25 +0000

    HW5:method getAlphabeticalMoviesAboveRating has been added

--- HW/MovieRatingsProcessor.java
+++ HW/MovieRatingsProcessor.java
@@ -26,13 +26,23 @@ public class MovieRatingsProcessor {
 
 	return alphabeticalMovieList;
     }
-
+    
+    /*
+     * given an input int rating, return a List of movie titles in alphabetical order, 
+     * where all movies in the List do not have any ratings less than or equal to rating 
+     * (hint: the PriorityQueue is a min-heap, meaning that the smallest rating is at the front of the queue!)
+     */
     public static List<String> getAlphabeticalMoviesAboveRating(TreeMap<String, PriorityQueue<Integer>> movieRatings,
 	    int rating) {
 
-	/* IMPLEMENT THIS METHOD! */
+	ArrayList<String> alphabeticalMoviesAboveRating = new ArrayList<String>();
+	if (movieRatings != null && !movieRatings.isEmpty() && rating >= 0) {
+	    for (String movietitle : movieRatings.keySet()) {
+		if(movieRatings.get(movietitle).peek()>rating) alphabeticalMoviesAboveRating.add(movietitle);
+	    }
+	}
 
-	return null; // this line is here only so this code will compile if you don't modify it
+	return alphabeticalMoviesAboveRating;
     }
 
     public static TreeMap<String, Integer> removeAllRatingsBelow(TreeMap<String, PriorityQueue<Integer>> movieRatings,
