5898e0453a30fc145bab55b8f3c348bb6938d2be
Roberto Monesi <mone.monesi.mm@gmail.com>
2018-03-10 16:21:50 +0000

    HW6: method minDistance added, but the helper method is not completed.

--- HW/BreadthFirstSearch.java
+++ HW/BreadthFirstSearch.java
@@ -1,9 +1,12 @@
 
-
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
+
+import com.sun.javafx.binding.StringFormatter;
 
 /*
  * SD2x Homework #6
@@ -12,41 +15,64 @@ import java.util.Set;
  */
 
 public class BreadthFirstSearch {
-	protected Set<Node> marked;
-	protected Graph graph;
+    protected Set<Node> marked;
+    protected Graph graph;
 
-	public BreadthFirstSearch(Graph graphToSearch) {
-		marked = new HashSet<Node>();
-		graph = graphToSearch;
+    public BreadthFirstSearch(Graph graphToSearch) {
+	marked = new HashSet<Node>();
+	graph = graphToSearch;
+    }
+
+    /**
+     * This method was discussed in the lesson
+     */
+    public boolean bfs(Node start, String elementToFind) {
+	if (!graph.containsNode(start)) {
+	    return false;
 	}
-	
-	/**
-	 * This method was discussed in the lesson
-	 */
-	public boolean bfs(Node start, String elementToFind) {
-		if (!graph.containsNode(start)) {
-				return false;
-		}
-		if (start.getElement().equals(elementToFind)) {
+	if (start.getElement().equals(elementToFind)) {
+	    return true;
+	}
+	Queue<Node> toExplore = new LinkedList<Node>();
+	marked.add(start);
+	toExplore.add(start);
+	while (!toExplore.isEmpty()) {
+	    Node current = toExplore.remove();
+	    for (Node neighbor : graph.getNodeNeighbors(current)) {
+		if (!marked.contains(neighbor)) {
+		    if (neighbor.getElement().equals(elementToFind)) {
 			return true;
+		    }
+		    marked.add(neighbor);
+		    toExplore.add(neighbor);
 		}
-		Queue<Node> toExplore = new LinkedList<Node>();
-		marked.add(start);
-		toExplore.add(start);
-		while (!toExplore.isEmpty()) {
-			Node current = toExplore.remove();
-			for (Node neighbor : graph.getNodeNeighbors(current)) {
-				if (!marked.contains(neighbor)) {
-					if (neighbor.getElement().equals(elementToFind)) {
-						return true;
-					}
-					marked.add(neighbor);
-					toExplore.add(neighbor);
-				}
-			}
-		}
-		return false;
+	    }
 	}
+	return false;
+    }
+
+    // Helper Method
+    public int findDistance(String start, String elementToFind) {
+
+	Node startNode = graph.getNode(start);
+	if (!graph.containsNode(startNode))
+	    return -1;
+	if (startNode.getElement().equals(elementToFind))
+	    return 0;
 	
+	Queue<Node> toExplore = new LinkedList<Node>(); //my queue with all the nodes to explore
+	Map<Node, Integer> distances = new HashMap<Node, Integer>();
+	
+	marked.add(startNode);
+	toExplore.add(startNode);
+	distances.put(startNode, 0);
+	
+	while(!toExplore.isEmpty()) {
+	    //TODO: implement this method and research way for find the minimumm distance.
+	    
+	}
+
+	return -1;
+    }
 
 }
--- HW/GraphUtils.java
+++ HW/GraphUtils.java
@@ -1,5 +1,4 @@
 
-
 import java.util.List;
 import java.util.Set;
 
@@ -11,27 +10,64 @@ import java.util.Set;
 
 public class GraphUtils {
 
-	public static int minDistance(Graph graph, String src, String dest) {
+    /*
+     * minDistance: Given a Graph, this method returns the shortest distance (in
+     * terms of number of edges) from the node labeled src to the node labeled dest.
+     * The method should return -1 for any invalid inputs, including: null values
+     * for the Graph, src, or dest; there is no node labeled src or dest in the
+     * graph; there is no path from src to dest. Keep in mind that this method does
+     * not just return the distance of any path from src to dest, it must be the
+     * shortest path.
+     */
 
-		/* IMPLEMENT THIS METHOD! */
-		
-		return -2; // this line is here only so this code will compile if you don't modify it
+    public static int minDistance(Graph graph, String src, String dest) {
+
+	if(graph==null || src == null || dest == null
+		|| !graph.containsElement(src) || !graph.containsElement(dest)) {
+	    return -1;
+	}else {
+	    int minDist = new BreadthFirstSearch(graph).findDistance(src,dest);
+	    return minDist;
 	}
-	
+    }
 
-	public static Set<String> nodesWithinDistance(Graph graph, String src, int distance) {
+    /*
+     * nodesWithinDistance: Given a Graph, this method returns a Set of the values
+     * of all nodes within the specified distance (in terms of number of edges) of
+     * the node labeled src, i.e. for which the minimum number of edges from src to
+     * that node is less than or equal to the specified distance. The value of the
+     * node itself should not be in the Set, even if there is an edge from the node
+     * to itself. The method should return null for any invalid inputs, including:
+     * null values for the Graph or src; there is no node labeled src in the graph;
+     * distance less than 1. However, if distance is greater than or equal to 1 and
+     * there are no nodes within that distance (meaning: src is the only node in the
+     * graph), the method should return an empty Set.
+     */
+    public static Set<String> nodesWithinDistance(Graph graph, String src, int distance) {
 
-		/* IMPLEMENT THIS METHOD! */
-		
-		return null; // this line is here only so this code will compile if you don't modify it
-	}
+	/* IMPLEMENT THIS METHOD! */
 
+	return null; // this line is here only so this code will compile if you don't modify it
+    }
 
-	public static boolean isHamiltonianPath(Graph g, List<String> values) {
+    /*
+     * isHamiltonianPath: Given a Graph, this method indicates whether the List of
+     * node values represents a Hamiltonian Path. A Hamiltonian Path is a valid path
+     * through the graph in which every node in the graph is visited exactly once,
+     * except for the start and end nodes, which are the same, so that it is a
+     * cycle. If the values in the input List represent a Hamiltonian Path, the
+     * method should return true, but the method should return false otherwise, e.g.
+     * if the path is not a cycle, if some nodes are not visited, if some nodes are
+     * visited more than once, if some values do not have corresponding nodes in the
+     * graph, if the input is not a valid path (i.e., there is a sequence of nodes
+     * in the List that are not connected by an edge), etc. The method should also
+     * return false if the input Graph or List is null.
+     */
+    public static boolean isHamiltonianPath(Graph g, List<String> values) {
 
-		/* IMPLEMENT THIS METHOD! */
-		
-		return true; // this line is here only so this code will compile if you don't modify it
-	}
-	
+	/* IMPLEMENT THIS METHOD! */
+
+	return true; // this line is here only so this code will compile if you don't modify it
+    }
+
 }
