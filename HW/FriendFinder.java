ce2fe955cd83f6ddf739fe6a8352cf51bfd27fed
Roberto Monesi <mone.monesi.mm@gmail.com>
2018-03-22 21:35:10 +0000

    HW10: all NullPointerExceptions  are solved. All tests passed

--- HW/FriendFinder.java
+++ HW/FriendFinder.java
@@ -10,57 +10,74 @@ import java.util.Set;
  */
 
 public class FriendFinder {
-	
-	protected ClassesDataSource classesDataSource;
-	protected StudentsDataSource studentsDataSource;
-	
-	public FriendFinder(ClassesDataSource cds, StudentsDataSource sds) {
-		classesDataSource = cds;
-		studentsDataSource = sds;
-	}
-	
-	
-	public Set<String> findClassmates(Student theStudent) {
-		
-		String name = theStudent.getName();
-		
-		// find the classes that this student is taking
-		List<String> myClasses = classesDataSource.getClasses(name);
-		
-		Set<String> classmates = new HashSet<String>();
-		
-		// use the classes to find the names of the students
-		for (String myClass : myClasses) {
-			// list all the students in the class
-			List<Student> students = studentsDataSource.getStudents(myClass);
-			
-			for (Student student : students) {
-				
-				// find the other classes that they're taking
-				List<String> theirClasses = classesDataSource.getClasses(student.getName());
-			
-				// see if all of the classes that they're taking are the same as the ones this student is taking
-				boolean same = true;
-				for (String c : myClasses) {
-					
-					if (theirClasses.contains(c) == false) {
-						same = false;
-						break;
-					}
-				}
-				if (same) {
-					if (student.getName().equals(name) == false && classmates.contains(student.getName()) == false) 
-						classmates.add(student.getName());
-				}
-			}
 
+    protected ClassesDataSource classesDataSource;
+    protected StudentsDataSource studentsDataSource;
+
+    public FriendFinder(ClassesDataSource cds, StudentsDataSource sds) {
+	classesDataSource = cds;
+	studentsDataSource = sds;
+    }
+
+    public Set<String> findClassmates(Student theStudent) {
+
+	if (theStudent == null)
+	    throw new IllegalArgumentException("theStudent is null");
+
+	if (classesDataSource == null || studentsDataSource == null)
+	    throw new IllegalStateException("classesDataSource or studentsDataSource are null");
+
+	String name = theStudent.getName();
+	if (name == null)
+	    throw new IllegalArgumentException("theStudent.getName returns some null");
+
+	// find the classes that this student is taking
+	List<String> myClasses = classesDataSource.getClasses(name);
+	// Return null if the input Student is not taking any classes
+	// or if there are no students taking the same classes as that student
+	if (myClasses == null || myClasses.isEmpty())
+	    return null;
+
+	Set<String> classmates = new HashSet<String>();
+
+	// use the classes to find the names of the students
+	for (String myClass : myClasses) {
+	    if (myClass == null)
+		continue;
+
+	    // list all the students in the class
+	    List<Student> students = studentsDataSource.getStudents(myClass);
+	    if (students == null || students.isEmpty()) continue;
+
+	    for (Student student : students) {
+		if(student == null || student.getName() == null) continue;
+
+		// find the other classes that they're taking
+		List<String> theirClasses = classesDataSource.getClasses(student.getName());
+		if(theirClasses == null || theirClasses.isEmpty()) continue;
+
+		// see if all of the classes that they're taking are the same as the ones this
+		// student is taking
+		boolean same = true;
+		for (String c : myClasses) {
+
+		    if (theirClasses.contains(c) == false) {
+			same = false;
+			break;
+		    }
 		}
-				
-		if (classmates.isEmpty()) { 
-			return null;
+		if (same) {
+		    if (student.getName().equals(name) == false && classmates.contains(student.getName()) == false)
+			classmates.add(student.getName());
 		}
-		else return classmates;
+	    }
+
 	}
-	
+
+	if (classmates.isEmpty()) {
+	    return null;
+	} else
+	    return classmates;
+    }
 
 }
